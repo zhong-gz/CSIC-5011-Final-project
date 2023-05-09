b2f85be0d52260dac87259cbd4837408845ea3a9
csgzzhong <csgzzhong@DESKTOP-H07UAEK.comp-dept-ad2.comp.hkbu.edu.hk>
2023-05-09 19:50:26 +0800

    final project

---
+++ Face_isomap.m
@@ -0,0 +1,31 @@
+clc
+clear
+close all
+
+load('face.mat');
+X=reshape(Y,[10304,33])';
+
+scale = 3;
+
+N = size(X,1);
+X = double(X);
+X = normalize(X,'norm');
+Y = flip(Y,1);
+
+for i = 1:3
+    faceimg(:,:,i,:) = Y;
+end
+
+mappedA = compute_mapping(X,'Isomap',2,5);
+
+figure(1);
+hold on
+for i = 1:N
+    image([mappedA(i,1) mappedA(i,1)+scale],...
+        [mappedA(i,2) mappedA(i,2)+scale*1.217]...
+        ,faceimg(:,:,:,i));
+    hold on
+end
+% xlim([0 33*scale])
+% ylim([-0.1 scale*1.217+0.1])
+% axis off
---
+++ Face_t_SNE.m
@@ -0,0 +1,32 @@
+clc
+clear
+close all
+
+load('face.mat');
+X=reshape(Y,[10304,33])';
+
+scale = 10;
+
+N = size(X,1);
+X = double(X);
+X = normalize(X,'norm');
+Y = flip(Y,1);
+
+for i = 1:3
+    faceimg(:,:,i,:) = Y;
+end
+
+mappedA = compute_mapping(X,'tSNE',2,30);
+scale = (max(mappedA,[],"all")-min(mappedA,[],"all"))*0.1;
+
+figure(1);
+hold on
+for i = 1:N
+    image([mappedA(i,1) mappedA(i,1)+scale],...
+        [mappedA(i,2) mappedA(i,2)+scale*1.217]...
+        ,faceimg(:,:,:,i));
+    hold on
+end
+% xlim([0 33*scale])
+% ylim([-0.1 scale*1.217+0.1])
+% axis off
